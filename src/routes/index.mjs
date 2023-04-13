6a6faf986f0f1077b6d18192cd33f8441e586bd7
Ralph Greaves <rgreaves@ycp.edu>
2023-04-12 23:17:33 -0400

    Update index.mjs

--- src/routes/index.mjs
+++ src/routes/index.mjs
@@ -15,7 +15,7 @@ router.route('/dashboard/event_applications')
     res.status(200).render('event_applications');
   })
   .post((req, res) => {
-    res.redirect(303, '/event_applications');
+    res.redirect(303, '/dashboard/event_applications');
   });
 
 export { router };
