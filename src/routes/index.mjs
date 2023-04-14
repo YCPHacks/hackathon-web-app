fb994f8b5d82c773f23f8891a6d986e6d8e45e8e
Kayla Hunt <khunt6@ycp.edu>
2023-04-13 22:11:20 -0400

    event app summary route

--- src/routes/index.mjs
+++ src/routes/index.mjs
@@ -3,21 +3,23 @@ import express from 'express';
 const router = express.Router();
 
 router.route('/')
-  .get((req, res) => {
-    res.status(200).render('index');
-  })
-  .post((req, res) => {
-    res.redirect(303, '/');
-  });
-
-router.route('/judging_criteria')
     .get((req, res) => {
-        res.status(200).render('judging_criteria');
+        res.status(200).render('index');
     })
+    .post((req, res) => {
+        res.redirect(303, '/');
+    });
+
+
 router.route('/gallery')
     .get((req, res) => {
         res.status(200).render('gallery');
     })
+router.route('/judging_criteria')
+    .get((req, res) => {
+        res.status(200).render('judging_criteria');
+    })
+
 router.route('/past_events')
     .get((req, res) => {
         res.status(200).render('past_events');
@@ -45,21 +47,26 @@ router.route('/dashboard')
     });
 
 router.route('/dashboard/event_applications')
-  .get((req, res) => {
-    res.status(200).render('event_applications');
-  })
-  .post((req, res) => {
-    res.redirect(303, '/dashboard/event_applications');
-  });
+    .get((req, res) => {
+        res.status(200).render('event_applications');
+    })
+    .post((req, res) => {
+        res.redirect(303, '/dashboard/event_applications');
+    });
+
+router.route('/dashboard/event_applications/event_applications_summary')
+    .get((req, res) => {
+        res.status(200).render('event_applications_summary');
+    })
 
 router.route('/dashboard/hardware_items')
-  .get((req, res) => {
-    res.status(200).render('hardware_items');
-  });
+    .get((req, res) => {
+        res.status(200).render('hardware_items');
+    });
 
 router.route('/dashboard/hardware_items/available')
-  .get((req, res) => {
-    res.status(200).render('hardware_items_available');
-  });
+    .get((req, res) => {
+        res.status(200).render('hardware_items_available');
+    });
 
-export { router };
+export {router};
