03a2ac5f374254e4cf4394ba9bf961a3fadc2405
Kayla Hunt <khunt6@ycp.edu>
2023-04-13 21:09:27 -0400

    dashboard, hardware_items pug

--- src/routes/index.mjs
+++ src/routes/index.mjs
@@ -10,6 +10,12 @@ router.route('/')
     res.redirect(303, '/');
   });
 
+router.route('/dashboard')
+    .get((req, res) => {
+        res.status(200).render('dashboard');
+    })
+
+
 router.route('/dashboard/event_applications')
   .get((req, res) => {
     res.status(200).render('event_applications');
@@ -17,5 +23,10 @@ router.route('/dashboard/event_applications')
   .post((req, res) => {
     res.redirect(303, '/dashboard/event_applications');
   });
+
+router.route('/dashboard/hardware_items')
+    .get((req, res) => {
+        res.status(200).render('hardware_items');
+    })
 
 export { router };
