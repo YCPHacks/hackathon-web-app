ee2940fe4ffd556969f0c453f63f13fd9895e455
Ralph Greaves <rgreaves@ycp.edu>
2023-04-25 21:11:33 -0400

    Attach public views to routes

--- src/routes/index.mjs
+++ src/routes/index.mjs
@@ -18,6 +18,22 @@ router.get('/', (req, res) => {
   res.status(200).render('index');
 });
 
+router.get('/gallery', (req, res) => {
+  res.status(200).render('gallery');
+});
+
+router.get('/judging_criteria', (req, res) => {
+  res.status(200).render('judging_criteria');
+});
+
+router.get('/past_events', (req, res) => {
+  res.status(200).render('past_events');
+});
+
+router.get('/sponsor', (req, res) => {
+  res.status(200).render('sponsor');
+});
+
 router.use('/dashboard', requiresAuth(), [
   event_applications,
   hardware_items
