b7e706f8d87bb8c9b0f79c1aaabe3a8a529bef2f
Kayla Hunt <khunt6@ycp.edu>
2023-04-25 20:50:42 -0400

    Create dashboard.js

---
+++ src/public/scripts/dashboard.js
@@ -0,0 +1,65 @@
+async function verify() {
+    'use strict'
+    // Fetch all the forms we want to apply custom Bootstrap validation styles to
+    const forms = document.querySelectorAll('.needs-validation')
+
+    // Loop over them and prevent submission
+    Array.from(forms).forEach(form => {
+        form.addEventListener('submit', event => {
+            if (!form.checkValidity()) {
+                event.preventDefault()
+                event.stopPropagation()
+            }
+
+            form.classList.add('was-validated')
+        }, false)
+    })
+}
+
+
+function edit(){
+    alert('Editing applicant form.')
+
+    const form = document.getElementById('editing').elements;
+    for (let i = 0; i < form.length; i++) {
+        // Disable all form controls
+        form[i].removeAttribute('readonly')
+    }
+    document.getElementById('update').removeAttribute('disabled')
+}
+function save(){
+
+}
+
+
+
+
+
+function show() {
+    const form = document.getElementById('allergyInput');
+    const submit = document.getElementById('tutorial');
+    form.setAttribute("required", "")
+    // submit.style.visibility = 'visible';
+    submit.style.display = 'block';
+}
+
+function hide() {
+    const form = document.getElementById('allergyInput');
+    const submit = document.getElementById('tutorial');
+    form.removeAttribute("required", "");
+    form.value = '';
+    submit.style.display = 'none';
+}
+
+function validateSize(input) {
+    const fileSize = input.files[0].size / 1024 / 1024; // in MiB
+    console.log(input.files[0].name);
+
+    if (fileSize > 2) {
+        alert('File size exceeds 2 MiB, please try uploading something smaller!');
+        input.value = null;
+        console.log("successfully reset");
+    } else {
+        // alert("its fine");
+    }
+}
