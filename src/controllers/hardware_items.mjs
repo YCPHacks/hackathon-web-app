2d81cda269ea3a6b76d8760f630149037e8b1fb9
Ralph Greaves <rgreaves@ycp.edu>
2023-04-27 01:12:33 -0400

    Update hardware_items.mjs

--- src/controllers/hardware_items.mjs
+++ src/controllers/hardware_items.mjs
@@ -15,7 +15,11 @@ export async function createHardwareItem(req, res, next) {
       'Authorization': `${accessToken.token_type} ${accessToken.access_token}`
     },
     body: JSON.stringify({
-      hardware_item: req.body.hardware_item
+      name: req.body.name,
+      link: req.body.link,
+      category: req.body.category,
+      status: req.body.status,
+      location: req.body.location
     })
   });
 
