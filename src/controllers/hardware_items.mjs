4cddca6c221d3705cc3188a9a19dc51e728a17fe
Ralph Greaves <rgreaves@ycp.edu>
2023-04-27 01:33:08 -0400

    Update hardware_items.mjs

--- src/controllers/hardware_items.mjs
+++ src/controllers/hardware_items.mjs
@@ -7,6 +7,8 @@
 import { fetch } from 'undici';
 
 export async function createHardwareItem(req, res, next) {
+  console.log(req);
+
   const accessToken = req.oidc.accessToken;
 
   const response = await fetch(`${process.env.API_BASE_URL}/hardware_items`, {
@@ -15,7 +17,13 @@ export async function createHardwareItem(req, res, next) {
       'Authorization': `${accessToken.token_type} ${accessToken.access_token}`,
       'Content-Type': 'application/json'
     },
-    body: JSON.stringify(req.body)
+    body: JSON.stringify({
+      name: req.body.name,
+      link: req.body.link,
+      category: req.body.category,
+      status: req.body.status,
+      location: req.body.location
+    })
   });
 
   return;
