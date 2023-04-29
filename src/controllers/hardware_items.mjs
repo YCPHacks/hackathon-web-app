0d816fdcca9662514d1b6f84525ff8e109d05e7d
Ralph Greaves <rgreaves@ycp.edu>
2023-04-29 13:03:48 -0400

    Update hardware_items.mjs

--- src/controllers/hardware_items.mjs
+++ src/controllers/hardware_items.mjs
@@ -7,34 +7,41 @@
 import { fetch } from 'undici';
 
 export async function createHardwareItem(req, res, next) {
-  console.log(req.body);
-  
+  const {
+    name,
+    link,
+    category,
+    status,
+    location
+  } = req.body;
+
   const accessToken = req.oidc.accessToken;
 
-  const response = await fetch(`${process.env.API_BASE_URL}/hardware_items`, {
+  return await fetch(`${process.env.API_BASE_URL}/hardware_items`, {
     method: 'POST',
     headers: {
-      'Authorization': `${accessToken.token_type} ${accessToken.access_token}`,
+      'Authorization':
+           `${accessToken.token_type} ${accessToken.access_token}`,
       'Content-Type': 'application/json'
     },
     body: JSON.stringify({
-      name: req.body.name,
-      link: req.body.link,
-      category: req.body.category,
-      status: req.body.status,
-      location: req.body.location
+      name,
+      link,
+      category,
+      status,
+      location
     })
   });
-
-  return;
 }
 
 export async function listHardwareItems(req, res, next) {
   const accessToken = req.oidc.accessToken;
 
-  const response = await fetch(`${process.env.API_BASE_URL}/hardware_items`, {
+  const response =
+      await fetch(`${process.env.API_BASE_URL}/hardware_items`, {
     headers: {
-      'Authorization': `${accessToken.token_type} ${accessToken.access_token}`
+      'Authorization':
+          `${accessToken.token_type} ${accessToken.access_token}`
     }
   });
 
