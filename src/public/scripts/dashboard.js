39c031a19eb45c44239ccb7b56ea2a0e6192d5f0
Kayla Hunt <khunt6@ycp.edu>
2023-05-01 13:02:43 -0400

    Update dashboard.js

--- src/public/scripts/dashboard.js
+++ src/public/scripts/dashboard.js
@@ -81,7 +81,6 @@ async function searchTable(){
     // Loop through all table rows, and hide those who don't match the search query
     for (i = 0; i < tr.length; i++) {
         td = tr[i].getElementsByTagName("td")[1];
-        console.log(td.value)
         if (td) {
             txtValue = td.textContent || td.innerText;
             if (txtValue.toUpperCase().indexOf(filter) > -1) {
