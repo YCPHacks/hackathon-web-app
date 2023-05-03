4556ebcbbbce727d9b6dc045f6230c4b73ad7df8
Kayla Hunt <khunt6@ycp.edu>
2023-05-03 12:58:50 -0400

    checkbox check

--- src/public/scripts/dashboard.js
+++ src/public/scripts/dashboard.js
@@ -16,6 +16,23 @@ async function verify() {
     })
 }
 
+
+const otherCheckbox = document.querySelector('#other');
+const otherText = document.querySelector('#otherValue');
+otherText.style.visibility = 'hidden';
+
+otherCheckbox.addEventListener('change', () => {
+    if (otherCheckbox.checked) {
+        otherText.style.visibility = 'visible';
+        otherText.value = '';
+    } else {
+        otherText.style.visibility = 'hidden';
+    }
+});
+
+
+
+
 function edit() {
     alert('Editing applicant form.')
 
