cbb27a1f8372dab67c19a415f6283ae3f1a5436c
Ralph Greaves <rgreaves@ycp.edu>
2023-05-04 03:52:38 -0400

    Update controller

--- src/controllers/events.mjs
+++ src/controllers/events.mjs
@@ -1,9 +1,10 @@
+import { fetch } from 'undici';
+
 export async function readEventApplication(access_token, event) {
-  const response = await
-        fetch(`${process.env.API_BASE_URL}/events/${event}/application`, {
+  const response = await fetch(
+        `${process.env.API_BASE_URL}/events/${event}/application`,
     headers: {
-      'Authorization':
-          `Bearer ${access_token}`
+      Authorization: `Bearer ${access_token}`
     }
   });
 
