9acb7fed2afdaa5fcc97d7531f3a840eef4ad08e
Ralph Greaves <rgreaves@ycp.edu>
2023-05-04 17:49:08 -0400

    Update event applications

--- src/routes/events.mjs
+++ src/routes/events.mjs
@@ -11,7 +11,15 @@ events.get('/:event/application', async (req, res) => {
       req.oidc.accessToken.access_token,
       req.params.event);
 
+  res.locals.metadata = {
+    event: req.params.event
+  };
+
   res.status(200).render('application');
+});
+
+events.post('/:event/application', async (req, res) => {
+  res.redirect(303, '/dashboard');
 });
 
 export { events };
