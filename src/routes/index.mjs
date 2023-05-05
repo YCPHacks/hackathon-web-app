765b70974c3b45b44209d86113e5fa7654ecd690
Kayla Hunt <khunt6@ycp.edu>
2023-05-05 06:02:11 -0400

    Public routes

--- src/routes/index.mjs
+++ src/routes/index.mjs
@@ -10,5 +10,19 @@ const router = express.Router();
 router.get('/', (req, res) => {
   res.send('home');
 });
-
+router.get('/', (req, res) => {
+  res.status(200).render('index');
+});
+router.get('/gallery', (req, res) => {
+  res.status(200).render('gallery');
+});
+router.get('/judging_criteria', (req, res) => {
+  res.status(200).render('judging_criteria');
+});
+router.get('/past_events', (req, res) => {
+  res.status(200).render('past_events');
+});
+router.get('/sponsor', (req, res) => {
+  res.status(200).render('sponsor');
+});
 export { router };
