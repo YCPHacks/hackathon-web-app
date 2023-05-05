9b715a756b7c99cb106b57e3a16cca541d64bf03
Kayla Hunt <khunt6@ycp.edu>
2023-05-05 06:53:39 -0400

    rendering dashboard

--- src/routes/dashboard.mjs
+++ src/routes/dashboard.mjs
@@ -7,6 +7,15 @@ const router = express.Router();
 router.get('/', (req, res) => {
     res.status(200).render('dashboard');
 });
+router.get('/hardware_items', (req, res) => {
+    res.status(200).render('hardware_items');
+});
+router.get('/check_in', (req, res) => {
+    res.status(200).render('check_in');
+});
+router.get('/event_applications', (req, res) => {
+    res.status(200).render('event_applications');
+});
 
 router.use('/events', events);
 
